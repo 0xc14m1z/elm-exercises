6b621b51c286342f7433529ef67c980f285892e2
Luca Ramundo <luca@ramundo.info>
2018-02-03 14:59:48 +0100

    Refine solution

--- leap/Leap.elm
+++ leap/Leap.elm
@@ -2,13 +2,4 @@ module Leap exposing (..)
 
 isLeapYear : Int -> Bool
 isLeapYear year =
-  if year % 4 == 0 then
-    if year % 100 == 0 then
-      if year % 400 == 0 then
-        True
-      else
-        False
-    else
-      True
-  else
-    False
+  year % 400 == 0 || year % 4 == 0 && year % 100 /= 0
