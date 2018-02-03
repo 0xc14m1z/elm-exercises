db664d7e0e477feccdfa01020bce4bd5e57e2288
Luca Ramundo <luca@ramundo.info>
2018-02-03 01:14:41 +0100

    Draft solution

--- bob/Bob.elm
+++ bob/Bob.elm
@@ -1,1 +1,18 @@
-module Bob exposing (..)
+module Bob exposing (hey)
+
+import Regex exposing (regex, contains)
+import String exposing (toUpper)
+
+question = regex "\?$"
+mute = regex "^\\s*$"
+
+hey : String
+hey statement =
+  if contains question statement then
+    "Sure."
+  else if statement == toUpper statement then
+    "Whoa, chill out!"
+  else if contains mute statement then
+    "Fine. Be that way!"
+  else
+    "Whatever."
