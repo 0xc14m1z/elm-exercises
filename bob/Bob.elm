64091a689a9d1f205660ac9d0589da392665e889
Luca Ramundo <luca@ramundo.info>
2018-02-03 12:35:06 +0100

    Solve exercise

--- bob/Bob.elm
+++ bob/Bob.elm
@@ -1,18 +1,31 @@
 module Bob exposing (hey)
 
 import Regex exposing (regex, contains)
-import String exposing (toUpper)
+import String exposing (isEmpty, toUpper)
+import Char exposing (isUpper, isLower)
 
-question = regex "\?$"
+question = regex "\\?$"
 mute = regex "^\\s*$"
 
-hey : String
+hey : String -> String
 hey statement =
-  if contains question statement then
-    "Sure."
-  else if statement == toUpper statement then
-    "Whoa, chill out!"
-  else if contains mute statement then
+  if contains mute statement then
     "Fine. Be that way!"
+  else if isShouting statement then
+    "Whoa, chill out!"
+  else if contains question statement then
+    "Sure."
   else
     "Whatever."
+
+isShouting : String -> Bool
+isShouting statement =
+  let
+    letters = String.filter isLetter statement
+    uppercaseLetters = String.toUpper letters
+  in
+    not (String.isEmpty letters) && letters == uppercaseLetters
+
+isLetter : Char -> Bool
+isLetter char =
+  Char.isUpper char || Char.isLower char
