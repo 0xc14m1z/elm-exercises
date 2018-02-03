b7972e9caa1b16f94e6496f36ce8572908ceeb63
Luca Ramundo <luca@ramundo.info>
2018-02-03 13:43:49 +0100

    Solve exercise

--- leap/Leap.elm
+++ leap/Leap.elm
@@ -1,1 +1,14 @@
 module Leap exposing (..)
+
+isLeapYear : Int -> Bool
+isLeapYear year =
+  if year % 4 == 0 then
+    if year % 100 == 0 then
+      if year % 400 == 0 then
+        True
+      else
+        False
+    else
+      True
+  else
+    False
