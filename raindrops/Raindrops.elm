a6c9a3898c43c8ef17f607fec4a5da87c995b5c1
Luca Ramundo <luca@ramundo.info>
2018-02-07 01:45:53 +0100

    Solve exercise

--- raindrops/Raindrops.elm
+++ raindrops/Raindrops.elm
@@ -1,1 +1,42 @@
 module Raindrops exposing (..)
+
+
+raindrops : Int -> String
+raindrops number =
+  let
+    words =
+      number
+        |> getFactors
+        |> speak
+  in
+    if List.isEmpty words then
+      toString number
+    else
+      String.join "" words
+
+
+speak : List Int -> List String
+speak factors =
+  factors
+    |> wordsMap
+    |> List.filter Tuple.second
+    |> List.map Tuple.first
+
+
+wordsMap : List Int -> List (String, Bool)
+wordsMap factors =
+  [ ("Pling", List.member 3 factors)
+  , ("Plang", List.member 5 factors)
+  , ("Plong", List.member 7 factors)
+  ]
+
+
+getFactors : Int -> List Int
+getFactors number =
+  List.range 1 number
+    |> List.filter (isFactor number)
+
+
+isFactor : Int -> Int -> Bool
+isFactor number potentialFactor =
+  number % potentialFactor == 0
